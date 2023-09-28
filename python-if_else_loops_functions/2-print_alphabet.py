ae4d2c671395d6acd6987f4a79cb88112cdb9194
Pepesaur96 <jjvazquez96@gmail.com>
2023-09-28 14:38:36 -0400

    task2.printformat.2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for ascii_value in range(97, 123):
-    print("{}".format(chr(ascii_value)), end="")
-
-print()
+for ascii_letters in range(97, 123):
+    print('{}'.format(chr(ascii_letters)), end="")
