b83ef7e27b46f91fc656d7f8b075424af7a9cbae
Pepesaur96 <jjvazquez96@gmail.com>
2023-09-28 14:30:46 -0400

    task2.single print

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for ascii_value in range(ord('a'), ord('z') + 1):
-    print(chr(ascii_value), end='')
+for ascii_value in range(97, 123):
+    print(chr(ascii_value), end="")
+
 print()
