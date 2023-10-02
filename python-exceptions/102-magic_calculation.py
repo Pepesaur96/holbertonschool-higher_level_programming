702ee34aec9009c2e0b58bd0fd6959aa83b64b10
Pepesaur96 <jjvazquez96@gmail.com>
2023-10-02 16:30:25 -0400

    task102.excepterrorfix

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    import sys
-    if a < b:
-        c = a + b
-        for i in range(4, 6):
-            try:
-                c = c + i
-            except:
-                sys.stderr.write("Exception: {}\n".format(err))
-                return c
-        return c
-    else:
-        return a - b
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise ValueError("Too far")
+            result += (a ** b) / i
+        except ZeroDivisionError:
+            result = b + a
+            break
+        except ValueError:
+            result = a + b
+            break
+    return result
