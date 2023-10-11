efe27063642ff6dd37d469c67c37cfa12ed9200f
Pepesaur96 <jjvazquez96@gmail.com>
2023-10-11 14:26:41 -0400

    bbranchadd + task0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module that adds two integers """
+
+
+def add_integer(a, b=98):
+    """ Function that adds two integers
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The addition of a and b
+    Raises:
+        TypeError: If a or b are not integers or floats
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
