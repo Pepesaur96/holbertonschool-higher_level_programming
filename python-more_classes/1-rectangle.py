418c3b573edcd2a3925fe7e1966e3837b0279672
Pepesaur96 <jjvazquez96@gmail.com>
2023-10-11 15:43:18 -0400

    task1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+Module that defines a rectangle by width and height
+"""
+
+
+class Rectangle:
+    """Defines class rectangle with private attributes width and height
+    Args:
+        width (int): width of rectangle
+        height (int): height of rectangle
+    Raises:
+        TypeError: if width or height is not an integer
+        ValueError: if width or height is less than zero
+    Methods:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)"""
+
+    def __init__(self, width, height):
+        """Initializes rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter for width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Setter for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Setter for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
