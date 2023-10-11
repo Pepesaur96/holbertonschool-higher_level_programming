15a0f3cfab1ff348288aff582807cd2cdf860e05
Pepesaur96 <jjvazquez96@gmail.com>
2023-10-11 16:04:26 -0400

    task1 elif to if fix

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -35,7 +35,7 @@ class Rectangle:
             ValueError: if width is less than 0"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
@@ -53,7 +53,7 @@ class Rectangle:
             ValueError: if height is less than 0"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
