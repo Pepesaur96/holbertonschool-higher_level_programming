b49220e060094f9aba13df9df363e6e5ced5de41
Pepesaur96 <jjvazquez96@gmail.com>
2023-10-16 16:51:46 -0400

    task1.pyhotfix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Class that inherits from list"""
+"""Class that inherits from list
+arguments: list"""
 
 
 class Mylist(list):
-    """Class that inherits from list"""
+    """Class that inherits from list
+    methods:
+        print_sorted(self)"""
 
     def print_sorted(self):
         """Function that prints the list, but sorted (ascending sort)"""
