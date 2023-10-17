84571e96341cce288bb7a106f5c730d150e8afd0
Pepesaur96 <jjvazquez96@gmail.com>
2023-10-17 13:09:32 -0400

    task4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that returns an object (Python data structure) represented by a JSON
+string"""
+
+
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string"""
+    import json
+    return json.loads(my_str)
