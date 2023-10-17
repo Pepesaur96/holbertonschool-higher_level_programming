7aea1fd97f929502f234cd3f63406759572d4015
Pepesaur96 <jjvazquez96@gmail.com>
2023-10-17 13:10:42 -0400

    task4.Pp8fix

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -4,6 +4,7 @@ string"""
 
 
 def from_json_string(my_str):
-    """Returns an object (Python data structure) represented by a JSON string"""
+    """Returns an object (Python data structure) represented by a
+    JSON string"""
     import json
     return json.loads(my_str)
