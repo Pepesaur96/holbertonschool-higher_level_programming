f8e873f10893d3d26a83a1e8ccf07acb7e475d12
Pepesaur96 <jjvazquez96@gmail.com>
2023-10-17 14:29:47 -0400

    task11.dictfix

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,31 +1,49 @@
 #!/usr/bin/python3
-"""Contains Student class"""
+"""Contains class Student
+that initializes public instance attributes first_name, last_name, and age,
+and has public method to_json that returns dictionary representation
+of requested attributes or all if none were requested
+"""
 
 
 class Student():
-    """Student class
-    attributes:
-        first_name (str): Student's first name.
-        Last_name (str): Student's last name.
-        age (int): Student's age.
-    methods:
-        to_json: returns dictionary representation of a Student instance
+    """Public Attributes:
+        first_name
+        last_name
+        age
+
+    Public Methods:
+        to_json: retrieves its dictionary representation
     """
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student.
+        """Initializes student with full name and age
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance"""
+        """Returns dictionary description with simple data structure
+        (list, dictionary, dictionary, string)
+        for JSON serialization of an object
+
+        Return:
+            Only return dict of attrs given to us
+            Return entire dict if no attrs given
+        """
         if attrs is None:
             return self.__dict__
         else:
-            dic = {}
+            dict = {}
             for att in attrs:
                 if att in self.__dict__.keys():
-                    dic[att] = self.__dict__[att]
-            return dic
+                    dict[att] = self.__dict__[att]
+            return dict
+
+    def reload_from_json(self, json):
+        """Return:
+            Transfer all attributes of json to self
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
