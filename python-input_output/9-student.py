43ac85762fff0139e73463658ef974e4a7572b8b
Pepesaur96 <jjvazquez96@gmail.com>
2023-10-17 14:05:21 -0400

    task9.(L)hotfix

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -8,7 +8,7 @@ class Student():
         to_json: returns dictionary representation of a Student instance
     """
 
-    def __init__(self, first_name, Last_name, age):
+    def __init__(self, first_name, last_name, age):
         """Initializes a new Student.
         Args:
             first_name (str): Student's first name.
@@ -20,7 +20,7 @@ class Student():
             age (int): Student's age.
         """
         self.first_name = first_name
-        self.Last_name = Last_name
+        self.Last_name = last_name
         self.age = age
 
     def to_json(self):
