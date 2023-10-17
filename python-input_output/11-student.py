b68464558fc779b8b8cf0b44fb849be27782b5b6
Pepesaur96 <jjvazquez96@gmail.com>
2023-10-17 14:21:38 -0400

    task11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Contains Student class"""
+
+
+class Student():
+    """Student class
+    attributes:
+        first_name (str): Student's first name.
+        Last_name (str): Student's last name.
+        age (int): Student's age.
+    methods:
+        to_json: returns dictionary representation of a Student instance
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            dict = {}
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    dict[att] = self.__dict__[att]
+            return dict
