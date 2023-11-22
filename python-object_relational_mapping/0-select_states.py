6cfef8730b82358facef65b29c8ba6a33d66825a
Pepesaur96 <jjvazquez96@gmail.com>
2023-11-22 11:07:06 -0400

    task0.exacutable test

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,8 +10,7 @@ if __name__ == "__main__":
         port=3306,
         user=argv[1],
         passwd=argv[2],
-        db=argv[3]
-    )
+        db=argv[3])
 
     # Create a cursor to execute SQL queries
     cursor = connection.cursor()
