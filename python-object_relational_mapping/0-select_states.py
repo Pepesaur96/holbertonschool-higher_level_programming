05f81863cdb044c97b43616b1802a7c0cc835f3e
Pepesaur96 <jjvazquez96@gmail.com>
2023-11-22 00:33:55 -0400

    task0.added args to connection

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    # Get MySQL credentials from command line arguments
-    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    # Connect to MySQL server running on localhost at port 3306
+    # Connect to MySQL server running on localhost at port 3306 and get MySQL credentials from command line arguments
     connection = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database_name
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
     )
 
     # Create a cursor to execute SQL queries
     cursor = connection.cursor()
 
     # Execute the SELECT query
-    query = "SELECT * FROM states ORDER BY id"
-    cursor.execute(query)
+    cursor.execute("SELECT * FROM states ORDER BY id")
 
     # Fetch all rows
-    rows = cursor.fetchall()
-
-    # Display the results
-    for row in rows:
+    for row in cursor.fetchall():
         print(row)
 
     # Close the cursor and connection
