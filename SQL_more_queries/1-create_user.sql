dbdb4df86259d1b61437f77a1dd5466f0193336a
Pepesaur96 <jjvazquez96@gmail.com>
2023-11-21 23:07:22 -0400

    task1.add all privalages on . and flush

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,8 @@
--- create database hbtn_0c_0
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges to apply the changes
+FLUSH PRIVILEGES;
