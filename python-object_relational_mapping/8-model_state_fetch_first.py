d768877defd43cbec5ea3ec0c9cf3603d2a99812
Pepesaur96 <jjvazquez96@gmail.com>
2023-11-22 12:58:04 -0400

    task8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" a script that prints the first State object from the database hbtn_0e_6_usa """
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    # Create engine that connects to the core (MySQL)
+    # The dialect is mysql, the driver is mysqldb
+    # user:password@host:port/database
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}"
+        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    # Generates new Session objects when called
+    Session = sessionmaker(bind=engine)
+
+    # instance of Session class
+    session = Session()
+
+    # Querying data from the State table
+    # Returns a list of objects
+    query = session.query(State).order_by(State.id).first()
+
+    # Print the id and name from the query
+    if query:
+        print("{}: {}".format(query.id, query.name))
+    else:
+        print("Nothing")
+
+    # Close the session
+    session.close()
