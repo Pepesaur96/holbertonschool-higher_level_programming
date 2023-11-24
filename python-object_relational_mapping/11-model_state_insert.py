3cc2e365f482c522b438531db868e4cbc158dda3
Pepesaur96 <jjvazquez96@gmail.com>
2023-11-24 11:06:18 -0400

    task11

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""a script that prints the State object
+with the name passed as argument from the
+database hbtn_0e_6_usa"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    # Create engine that connects to the core
+    # (MySQL) The dialect is mysql, the driver
+    # is mysqldb user:password@host:port/database
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}"
+        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    # Create all the objects in the database
+    Base.metadata.create_all(engine)
+
+    # Generates new Session objects when called
+    Session = sessionmaker(bind=engine)
+
+    # instance of Session class
+    session = Session()
+
+    # Create a new state
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
+
+    # Close the session
+    session.close()
