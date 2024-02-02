f7c5c54e3365ddf83ea2074fa7d997fd150fd79b
Pepesaur96 <jjvazquez96@gmail.com>
2024-02-02 13:59:13 -0400

    Task0.Script to color header text

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,5 @@
+// Select the header element using document.querySelector
+const header = document.querySelector('header');
+
+// Set the color of the text to red (#FF0000)
+header.style.color = '#FF0000';
